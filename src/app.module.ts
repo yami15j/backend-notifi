6d510cbb6944cfbbf97bc57426bb975154fe7e67
zyami15 <zyjumbo@sudamericano.edu.ec>
2025-06-06 07:30:18 -0500

    primer commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres', // tu usuario de postgres
-      password: '12345',    // tu contraseña
-      database: 'notification_db',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    NotificationsModule,
-
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+TypeOrmModule.forRoot({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    port: 5432,
+    username: 'postgres',
+    password: 'UncArnNeYNmUUZyTbrtimpwiqOraxTyD',
+    database: 'railway',
+    entities: [__dirname + `/**/*.entity{.ts,.js}`],
+    synchronize: true,
+  }), NotificationsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true }); // 👈 habilita CORS aquí
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {cors:true});
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
